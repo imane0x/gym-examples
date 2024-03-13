cb3a1bdf2b2b87a2fb24736c9e9b2c5dc45132d6
Imane Ouada <86524680+imane0x@users.noreply.github.com>
2024-03-13 14:57:44 +0000

    Update __init__.py

--- gym_examples/__init__.py
+++ gym_examples/__init__.py
@@ -1,9 +1,9 @@
 from gym.envs.registration import register
 
-register(
-    id="gym_examples/GridWorld-v0",
-    entry_point="gym_examples.envs:GridWorldEnv",
-)
+# register(
+#     id="gym_examples/GridWorld-v0",
+#     entry_point="gym_examples.envs:GridWorldEnv",
+# )
 register(
     id="gym_examples/TSP-v0",
     entry_point="gym_examples.envs:TSPEnvironment",
