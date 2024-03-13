49e61a8db0687402a596593f847c1addf6b14f74
Imane Ouada <86524680+imane0x@users.noreply.github.com>
2024-03-13 14:35:22 +0000

    Update __init__.py

--- gym_examples/__init__.py
+++ gym_examples/__init__.py
@@ -4,3 +4,7 @@ register(
     id="gym_examples/GridWorld-v0",
     entry_point="gym_examples.envs:GridWorldEnv",
 )
+register(
+    id="gym_examples/TSP-v0",
+    entry_point="gym_examples.envs:TSPEnvironment",
+)
