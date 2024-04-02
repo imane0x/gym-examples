bfbbaf6d6a1de17d3c094c0227a7b7be9c305e75
Imane Ouada <86524680+imane0x@users.noreply.github.com>
2024-04-02 13:12:22 +0000

    Update tsp.py

--- gym_examples/envs/tsp.py
+++ gym_examples/envs/tsp.py
@@ -1,8 +1,75 @@
+# import numpy as np
+# import gym
+# from gym import spaces
+# import matplotlib.pyplot as plt
+# #from gym.envs.registration import register
+
+# class TSPEnvironment(gym.Env):
+#     def __init__(self, num_cities):
+#         super(TSPEnvironment, self).__init__()
+#         self.num_cities = num_cities
+#         self.city_coordinates = np.random.rand(num_cities, 2)   # Random 2D coordinates for cities
+#         self.distance_matrix = self.calculate_distance_matrix()
+
+#         self.current_city = 0
+#         self.visited_cities = set([0])  # Set to keep track of visited cities
+
+#         # Gym spaces
+#         self.action_space = spaces.Discrete(num_cities)
+#         self.observation_space = spaces.Box(low=0, high=100, shape=(2,), dtype=np.float32)
+
+#         # Visualization
+#         self.fig, self.ax = plt.subplots()
+
+#     def reset(self):
+#         self.current_city = 0
+#         self.visited_cities = set([0])
+#         return self.city_coordinates[self.current_city]
+
+#     def calculate_distance_matrix(self):
+#         distances = np.zeros((self.num_cities, self.num_cities))
+#         for i in range(self.num_cities):
+#             for j in range(self.num_cities):
+#                 distances[i, j] = np.linalg.norm(self.city_coordinates[i] - self.city_coordinates[j])
+#         return distances
+
+#     def step(self, action):
+#         if action not in self.visited_cities:
+#             reward = self.distance_matrix[self.current_city, action]
+#             self.visited_cities.add(action)
+#             self.current_city = action
+#         else:
+#             reward = 0  # Penalize revisiting a city
+
+#         done = len(self.visited_cities) == self.num_cities
+#         next_state = self.city_coordinates[self.current_city]
+
+#         self.ax.clear()
+#         self.ax.scatter(self.city_coordinates[:, 0], self.city_coordinates[:, 1], color='blue', label='Cities')
+#         self.ax.scatter(*next_state, color='green' if not done else 'red', marker='*' if not done else 'x', s=200, label='Next City')
+#         if not done:
+#             self.ax.scatter(*self.city_coordinates[[list(self.visited_cities)[0]]].T, color='orange', marker='o', s=200, label='Start City')
+#         self.ax.legend()
+#         plt.pause(1)  # To give time for visualization
+
+#         return next_state, reward, done, {}
+
+# def render(self, mode='human'):
+#     plt.figure(figsize=(6, 6))
+#     plt.scatter(self.city_coordinates[:, 0], self.city_coordinates[:, 1], color='blue', label='Cities')
+#     plt.scatter(self.city_coordinates[self.current_city, 0], self.city_coordinates[self.current_city, 1], color='red', label='Current City')
+#     plt.legend()
+#     plt.title('Traveling Salesman Problem')
+#     plt.xlabel('X-coordinate')
+#     plt.ylabel('Y-coordinate')
+#     plt.grid(True)
+#     plt.show()
+
+
 import numpy as np
 import gym
 from gym import spaces
 import matplotlib.pyplot as plt
-#from gym.envs.registration import register
 
 class TSPEnvironment(gym.Env):
     def __init__(self, num_cities):
@@ -16,7 +83,7 @@ class TSPEnvironment(gym.Env):
 
         # Gym spaces
         self.action_space = spaces.Discrete(num_cities)
-        self.observation_space = spaces.Box(low=0, high=100, shape=(2,), dtype=np.float32)
+        self.observation_space = spaces.Discrete(num_cities)  # Changed observation space
 
         # Visualization
         self.fig, self.ax = plt.subplots()
@@ -24,7 +91,7 @@ class TSPEnvironment(gym.Env):
     def reset(self):
         self.current_city = 0
         self.visited_cities = set([0])
-        return self.city_coordinates[self.current_city]
+        return self.current_city  # Return index of current city
 
     def calculate_distance_matrix(self):
         distances = np.zeros((self.num_cities, self.num_cities))
@@ -42,27 +109,39 @@ class TSPEnvironment(gym.Env):
             reward = 0  # Penalize revisiting a city
 
         done = len(self.visited_cities) == self.num_cities
-        next_state = self.city_coordinates[self.current_city]
+        next_state = self.current_city  # Next state is the index of the next city
 
         self.ax.clear()
         self.ax.scatter(self.city_coordinates[:, 0], self.city_coordinates[:, 1], color='blue', label='Cities')
-        self.ax.scatter(*next_state, color='green' if not done else 'red', marker='*' if not done else 'x', s=200, label='Next City')
+        self.ax.scatter(*self.city_coordinates[self.current_city], color='green' if not done else 'red', marker='*' if not done else 'x', s=200, label='Next City')
         if not done:
             self.ax.scatter(*self.city_coordinates[[list(self.visited_cities)[0]]].T, color='orange', marker='o', s=200, label='Start City')
         self.ax.legend()
-        plt.pause(1)  # To give time for visualization
+        plt.pause(0.001)  # Short pause for visualization
 
         return next_state, reward, done, {}
 
-def render(self, mode='human'):
-    plt.figure(figsize=(6, 6))
-    plt.scatter(self.city_coordinates[:, 0], self.city_coordinates[:, 1], color='blue', label='Cities')
-    plt.scatter(self.city_coordinates[self.current_city, 0], self.city_coordinates[self.current_city, 1], color='red', label='Current City')
-    plt.legend()
-    plt.title('Traveling Salesman Problem')
-    plt.xlabel('X-coordinate')
-    plt.ylabel('Y-coordinate')
-    plt.grid(True)
-    plt.show()
+    def render(self, mode='human'):
+        plt.figure(figsize=(6, 6))
+        plt.scatter(self.city_coordinates[:, 0], self.city_coordinates[:, 1], color='blue', label='Cities')
+        plt.scatter(self.city_coordinates[self.current_city, 0], self.city_coordinates[self.current_city, 1], color='red', label='Current City')
+        plt.legend()
+        plt.title('Traveling Salesman Problem')
+        plt.xlabel('X-coordinate')
+        plt.ylabel('Y-coordinate')
+        plt.grid(True)
+        plt.show()
+
+# Example usage:
+env = TSPEnvironment(num_cities=10)
+observation = env.reset()
+env.render()
+
+for _ in range(9):
+    action = env.action_space.sample()  # Random action for demonstration
+    observation, reward, done, _ = env.step(action)
+    env.render()
+
+env.close()  # Close the environment
 
 
