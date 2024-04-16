811fd70a108405181077886c87c41b36a964346a
Imane Ouada <86524680+imane0x@users.noreply.github.com>
2024-04-16 10:23:55 +0100

    Update tsp.py

--- gym_examples/envs/tsp.py
+++ gym_examples/envs/tsp.py
@@ -2,6 +2,7 @@ import numpy as np
 import gym
 from gym import spaces
 import matplotlib.pyplot as plt
+import itertools
 #from gym.envs.registration import register
 
 class TSPEnvironment(gym.Env):
@@ -45,12 +46,27 @@ class TSPEnvironment(gym.Env):
         # next_state = self.city_coordinates[self.current_city]
         next_state = self.current_city
 
-
-
         return next_state, reward, done, {}
+    def get_optimal_tour(self):
+        points = list(range(self.num_cities))
+        all_distances = self.distance_matrix
+        A = {(frozenset([0, idx + 1]), idx + 1): (dist, [0, idx + 1]) for idx, dist in enumerate(all_distances[0][1:])}
+        cnt = len(points)
+        for m in range(2, cnt):
+            B = {}
+            for S in [frozenset(C) | {0} for C in itertools.combinations(range(1, cnt), m)]:
+                for j in S - {0}:
+                    B[(S, j)] = min([(A[(S - {j}, k)][0] + all_distances[k][j], A[(S - {j}, k)][1] + [j]) for k in S if
+                                     k != 0 and k != j])
+            A = B
+        res = min([(A[d][0] + all_distances[0][d[1]], A[d][1]) for d in iter(A)])
+        tour = np.array(res[1])
+        return tour
 
-
-
+    def get_optimal_reward(self):
+        optimal_reward = sum(self.distance_matrix[self.optimal_tour[i], self.optimal_tour[i+1]] for i in range(len(self.optimal_tour)-1))
+        optimal_reward += self.distance_matrix[self.optimal_tour[-1], self.optimal_tour[0]]  
+        return optimal_reward
 
 
 
