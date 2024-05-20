a06a5211a197884863e2898a15818c3cf2047181
Imane Ouada <86524680+imane0x@users.noreply.github.com>
2024-05-20 12:31:41 +0100

    Update grid_world.py

--- gym_examples/envs/grid_world.py
+++ gym_examples/envs/grid_world.py
@@ -7,7 +7,7 @@ import numpy as np
 class GridWorldEnv(gym.Env):
     metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 4}
 
-    def __init__(self, render_mode=None, size=5):
+    def __init__(self, render_mode="human", size=5):
         self.size = size  # The size of the square grid
         self.window_size = 512  # The size of the PyGame window
 
