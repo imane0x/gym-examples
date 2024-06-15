864faff629e21a500e3628103d867eede9fdb2ac
Imane Ouada <86524680+imane0x@users.noreply.github.com>
2024-06-15 13:13:48 +0100

    Update __init__.py

--- gym_examples/envs/__init__.py
+++ gym_examples/envs/__init__.py
@@ -1,3 +1,4 @@
 from gym_examples.envs.grid_world import GridWorldEnv
 from gym_examples.envs.tsp import TSPEnvironment
 from gym_examples.envs.Nqueens import NQueensEnv
+from gym_examples.envs.NQ import NQ
